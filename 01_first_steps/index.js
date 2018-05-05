432f25476381d3bdb9b4e647ed3af1de848e94d6
Wenceslao Negrete <vampay@digitaldealers.mx>
2018-05-05 11:08:51 -0500

    node first steps

---
+++ 01_first_steps/index.js
@@ -0,0 +1,7 @@
+//Declare a const named num
+const num1=10;
+const num2=20;
+
+//Declare a function named add that calculates the addition of num1 plus num2
+const add= (num1, num2) => (num1 + num2);
+console.log(add(1,2));
