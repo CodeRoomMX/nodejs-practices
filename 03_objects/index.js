f5dd9548f88dbce1549d1c3fc824afdd2529fee2
Wenceslao Negrete <vampay@digitaldealers.mx>
2018-05-05 13:32:57 -0500

    03 objects

---
+++ 03_objects/index.js
@@ -0,0 +1,10 @@
+class Warrior{
+  constructor(name, health, power, armor){
+    
+  }
+};
+
+let w1 = new Warrior('Goku', 20,10,5);
+let w2 = new Warrior('Freezer', 20,10,3);
+
+console.log(w1, w2);
